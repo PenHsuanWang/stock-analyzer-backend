fcdb79237c46eae9466f6bb4cc49df3fd0312102
PenHsuanWang <willy118116@gmail.com>
2023-11-09 21:12:43 +0800

    integrated correlation into stock analysis serving app

--- src/webapp/router/stock_analysis_serving_app_router.py
+++ src/webapp/router/stock_analysis_serving_app_router.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Depends, Body, HTTPException
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
 from webapp.serving_app.stock_analysis_serving_app import get_stock_analysis_serving_app, StockAnalysisServingApp
 
 router = APIRouter()
+
 
 class ComputeMARequest(BaseModel):
     prefix: str
@@ -13,11 +15,11 @@ class ComputeMARequest(BaseModel):
     window_sizes: list[int]
 
 
-class ComputeDRRequest(BaseModel):
-    prefix: str
-    stock_id: str
+class ComputeCorrelationRequest(BaseModel):
+    stock_ids: list[str]
     start_date: str
     end_date: str
+    metric: str
 
 
 # Define a dependency that will provide an instance of StockAnalysisServingApp
@@ -48,25 +50,32 @@ def compute_and_store_moving_average(
         raise HTTPException(status_code=500, detail=str(e))
 
 
-# @router.post("/stock_data/compute_and_store_daily_return")
-# def compute_and_store_daily_return(
-#     request: ComputeDRRequest = Body(...),
-#     app: StockAnalysisServingApp = Depends(get_serving_app_dependency)
-# ):
-#     """
-#     Endpoint to compute daily returns and update Redis store.
-#
-#     :param request: Request body with stock details.
-#     :param app: Instance of StockAnalysisServingApp provided by Depends.
-#     :return: Confirmation message.
-#     """
-#     try:
-#         app.calculating_daily_return_and_saved_as_analyzed_prefix(
-#             prefix=request.prefix,
-#             stock_id=request.stock_id,
-#             start_date=request.start_date,
-#             end_date=request.end_date
-#         )
-#         return {"message": "Daily returns computed and stored into Redis successfully."}
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
+@router.post("/stock_data/calculate_correlation")
+def calculate_correlation(
+    request: ComputeCorrelationRequest = Body(...),
+    app: StockAnalysisServingApp = Depends(get_serving_app_dependency)
+):
+    """
+    Endpoint to calculate correlation between assets and optionally store the result.
+
+    :param request: Request body with stock IDs, dates, and metric for correlation.
+    :param app: Instance of StockAnalysisServingApp provided by Depends.
+    :return: JSON response with correlation data.
+    """
+    try:
+        correlation_df = app.calculate_and_store_correlation(
+            stock_ids=request.stock_ids,
+            start_date=request.start_date,
+            end_date=request.end_date,
+            metric=request.metric
+        )
+        return correlation_df.to_dict()
+    except HTTPException as http_exc:
+        raise http_exc
+    except Exception as e:
+        return JSONResponse(
+            status_code=500,
+            content={
+                "message": f"An error occurred while computing asset correlations. Error details: {str(e)}"
+            }
+        )
