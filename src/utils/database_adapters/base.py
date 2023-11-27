5464d3bf706750e0ffec33815ccbb5775463d46b
PenHsuanWang <willy118116@gmail.com>
2023-11-27 23:13:25 +0800

    rename module and package

--- src/utils/database_adapters/base.py
+++ src/utils/database_adapters/base.py
@@ -1,7 +1,7 @@
 # utils/database_adaptors/base.py
 
 from abc import ABC, abstractmethod
-
+from typing import Any, List, Optional
 
 class AbstractDatabaseAdapter(ABC):
     """
@@ -9,22 +9,24 @@ class AbstractDatabaseAdapter(ABC):
     """
 
     @abstractmethod
-    def set_data(self, key: str, value: str):
+    def set_data(self, key: str, value: Any):
         """
         Store data in the database.
 
         :param key: The key under which the data should be stored.
-        :param value: The data to store.
+        :param value: The data to store. The type of data can be any,
+                      as it depends on the specific database adapter.
         """
         raise NotImplementedError
 
     @abstractmethod
-    def get_data(self, key: str) -> str:
+    def get_data(self, key: str) -> Optional[Any]:
         """
         Retrieve data from the database.
 
         :param key: The key of the data to retrieve.
-        :return: The data as a string.
+        :return: The data as a specific type, depending on the database adapter.
+                 Returns None if no data is found.
         """
         raise NotImplementedError
 
@@ -49,11 +51,11 @@ class AbstractDatabaseAdapter(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def keys(self, pattern: str = None) -> list:
+    def keys(self, pattern: Optional[str] = None) -> List[str]:
         """
         Retrieve a list of keys from the database matching a pattern.
 
-        :param pattern: The pattern to match.
-        :return: A list of keys.
+        :param pattern: The pattern to match. If None, all keys may be returned.
+        :return: A list of keys as strings.
         """
         raise NotImplementedError
